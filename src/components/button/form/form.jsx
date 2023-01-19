faf8c43beca0dcf49ef6c171fdd407f1bc4d4a94
evgeniyaili <ilinaevgeniya@mail.ru>
2023-01-20 00:02:38 +0300

    12

--- src/components/button/form/form.jsx
+++ src/components/button/form/form.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { hooks } from '../../../hooks/hooks';
 import './form.css';
 
 const Form = () => {
     const [country, setCountry] = useState('');
     const [address, setAddress] = useState('');
-    const [gender, setGender] = useState('Men');
+    const [gender, setGender] = useState('Man');
+    const {tg} = hooks();
+
+    useEffect(()=> {
+        tg.MainButton.setParams({
+            text:'Send data'
+    })
+    }, [])
+    useEffect(()=>{
+        if (!address || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, address])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
@@ -23,7 +38,7 @@ const Form = () => {
             <input type="text" placeholder={'Country'} value={country} onChange={onChangeCountry}/>
             <input type="text" placeholder={'Address'} value={address} onChange={onChangeAddress}/>
             <select className={'select'} value={gender} onChange={onChangeGender}>
-                <option value={'Men'}>Men</option>
+                <option value={'Man'}>Men</option>
                 <option value={'Woman'}>Woman</option>
             </select>
         </div>
