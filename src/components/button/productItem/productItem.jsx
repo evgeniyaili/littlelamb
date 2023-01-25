869cbe67d0b6604893b8750702bf5844b5ccccac
evgeniyaili <ilinaevgeniya@mail.ru>
2023-01-25 22:39:28 +0300

    10

--- src/components/button/productItem/productItem.jsx
+++ src/components/button/productItem/productItem.jsx
@@ -8,7 +8,9 @@ const ProductItem = ({product, className, onAdd}) => {
     }
     return (
         <div className={'product' + className}>
-            <div className={'img'}/>
+            <div className={'img'}>
+                <img src="../media/pic2.webp" alt="sneaker 1"/>
+            </div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
